ef83880ad3acc3c10d5788f8a33c45606bc9a519
oroszdavid9999 <oroszdavid9999@gmail.com>
2022-05-13 11:20:02 +0200

    Test added

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -3,38 +3,47 @@ package hu.bme.mit.spaceship;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 
 import static org.mockito.Mockito.*;
 
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore firstMock = Mockito.mock(TorpedoStore.class);
+  private TorpedoStore secondMock = Mockito.mock(TorpedoStore.class);
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    Mockito.mock(TorpedoStore.class);
+    this.ship = new GT4500( firstMock, secondMock);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+    Mockito.when(firstMock.fire(1)).thenReturn(true);
+    Mockito.when(secondMock.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(firstMock, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+    Mockito.when(firstMock.fire(1)).thenReturn(true);
+    Mockito.when(secondMock.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(firstMock, times(1)).fire(1);
   }
 
 }
